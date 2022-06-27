bc39fb599f994b5f37b8a134009d05e28fd4c84f
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-06-27 08:18:56 +0300

    fix data_manager

--- data_manager.py
+++ data_manager.py
@@ -1,14 +1,17 @@
 from unicodedata import name
 import face_recognition
-import cv2
-from PIL import ImageFont, ImageDraw, Image
-import numpy as np
 import os
 from pymongo import MongoClient
+
+#Объявление констант
+#Путь к директории файлов
+DIR_PATH = "files"
 
 class DAO:
     known_face_encodings = []
     known_face_names = []
+    known_paths = []
+    
     def __init__(self):
         self.faces= self.connectDB()
         
@@ -22,27 +25,34 @@ class DAO:
     def getData(self):
         known_face_encodings = []
         known_face_names = []
+        known_paths=[]
 
         #Записывает в бд декодированные лица
         for i in self.faces.find():
             known_face_encodings.append(i["embedding"])
             known_face_names.append(i["name"])
-        return known_face_names, known_face_encodings
+            known_paths.append(i["imgpath"])
+        return known_face_names, known_face_encodings,known_paths
 
 
 
-    def insertIfNotExist(self, names, encodings):
+    def insertIfNotExist(self, names, encodings,paths):
         for i in range(len(names)):
-            self.faces.update_one({"name": names[i]}, {"$set":{"embedding": encodings[i].tolist()} },  upsert= True)
+            self.faces.update_one({"name": names[i]}, {
+                "$set":{"embedding": encodings[i].tolist()} 
+                },  upsert= True)
+            self.faces.update_one({"name": names[i]},{"$set":{"imgpath":paths[i]}}, upsert=True)
 
 class Data:
         def loadDataFromDir(self):
             known_face_encodings = []
             known_face_names = []
+            known_paths =[]
+            
             # Load a sample picture and learn how to recognize it.
-            for img in os.listdir("C:\\Users\\Nursultan\\Desktop\\recognition test\\files"):
-                imgFile=face_recognition.load_image_file(os.path.join("C:\\Users\\Nursultan\\Desktop\\recognition test\\files",img))
+            for img in os.listdir(DIR_PATH):
+                imgFile=face_recognition.load_image_file(os.path.join(DIR_PATH,img))
+                known_paths.append(os.path.join(DIR_PATH,img))
                 known_face_encodings.append(face_recognition.face_encodings(imgFile)[0])
-                nameImg = img.split(".")
-                known_face_names.append(nameImg[0]) 
-            return known_face_names, known_face_encodings
+                known_face_names.append(img.split(".")[0]) 
+            return known_face_names, known_face_encodings, known_paths
